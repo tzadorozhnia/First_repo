56746264c08760c6b6d21f48c5cec6952c45b928
zadorozhnyayatatyana <zadorozhnyayatatyana@gmail.com>
2025-09-09 06:37:22 +0300

    second commit0

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Hello Git")
+def print_max(a: int, b: int):
+    if a > b:
+        print(a, 'максимально')
+    elif a == b:
+        print(a, 'дорівнює', b)
+    else:
+        print(b, 'максимально')
+
+print_max(3, 4)
